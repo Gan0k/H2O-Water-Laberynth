902ddcbf62e300a303ca2433dcc5910e1380f4cc
Guido Arnau <guido.arnau@gmail.com>
2015-03-22 02:17:44 +0100

    NFC works, reads tags, bunch of useless code

--- src/android/src/com/mygdx/sorryFib/android/AndroidLauncher.java
+++ src/android/src/com/mygdx/sorryFib/android/AndroidLauncher.java
@@ -1,22 +1,22 @@
 package com.mygdx.sorryFib.android;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
- 
-import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentFilter.MalformedMimeTypeException;
+import java.util.Locale;
+import java.util.List;
+import java.nio.charset.Charset;
+
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.os.AsyncTask;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Parcelable;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
 import android.widget.Toast;
+import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -24,147 +24,241 @@ import com.mygdx.sorryFib.Game;
 
 public class AndroidLauncher extends AndroidApplication {
 
-	private NfcAdapter mNfcAdapter;
+    private NfcAdapter mAdapter;
+    private PendingIntent mPendingIntent;
+    private NdefMessage mNdefPushMessage;
 
 	public static final String MIME_TEXT_PLAIN = "text/plain";
-	public static final String TAG = "NfcDemo";
+	public static final String TAG = "OMGZWATER";
 
 	private Game game;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		resolveIntent(getIntent());
 
-		mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-
-		if (mNfcAdapter == null) {
-			Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+        mAdapter = NfcAdapter.getDefaultAdapter(this);
+		if (mAdapter == null) {
+			Toast.makeText(this, R.string.no_nfc, Toast.LENGTH_LONG).show();
 			finish();
 			return;
 		}
 	 
-		if (!mNfcAdapter.isEnabled()) {
-			Toast.makeText(this, "NFC Disabled, please enable.", Toast.LENGTH_LONG).show();
+		if (!mAdapter.isEnabled()) {
+			showNFCDisabledToast();
 		} else {
 			Log.i(TAG,"NFC systems enabled!");
 		}
 
-		handleIntent(getIntent());
+		mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+		mNdefPushMessage = new NdefMessage(new NdefRecord[] { newTextRecord(
+                "Message from NFC Reader :-)", Locale.ENGLISH, true) });
 
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		game = new Game();
 		initialize(game, config);
 	}
 
-	private void handleIntent(Intent intent) {
-		Log.i(TAG,Integer.toString(times));
-		String action = intent.getAction();
-		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-			String type = intent.getType();
-			if (MIME_TEXT_PLAIN.equals(type)) {
-				Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-				new NdefReaderTask().execute(tag);
-			} 
-			else Log.d(TAG, "Wrong mime type: " + type);
-		} 
-		else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
-			Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-			String[] techList = tag.getTechList();
-			String searchedTech = Ndef.class.getName();
-			 
-			for (String tech : techList) {
-				if (searchedTech.equals(tech)) {
-					new NdefReaderTask().execute(tag);
-					break;
-				}
-			}
-		}
+	private void showNFCDisabledToast () {
+		Toast.makeText(this, R.string.enable_nfc, Toast.LENGTH_LONG).show();
 	}
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		setupForegroundDispatch(this, mNfcAdapter);
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mAdapter != null) {
+            if (!mAdapter.isEnabled()) {
+                showNFCDisabledToast();
+            }
+            mAdapter.enableForegroundDispatch(this, mPendingIntent, null, null);
+            mAdapter.enableForegroundNdefPush(this, mNdefPushMessage);
+        }
+    }
 
-	@Override
-	protected void onPause() {
-		stopForegroundDispatch(this, mNfcAdapter);  
-		super.onPause();
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAdapter != null) {
+            mAdapter.disableForegroundDispatch(this);
+            mAdapter.disableForegroundNdefPush(this);
+        }
+    }
 
-	@Override
-	protected void onNewIntent(Intent intent) { 
-		handleIntent(intent);
-	}
+	private NdefRecord newTextRecord(String text, Locale locale, boolean encodeInUtf8) {
+        byte[] langBytes = locale.getLanguage().getBytes(Charset.forName("US-ASCII"));
 
-	public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-		final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
-		intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
- 
-		final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
- 
-		IntentFilter[] filters = new IntentFilter[1];
-		String[][] techList = new String[][]{};
- 
-		filters[0] = new IntentFilter();
-		filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
-		filters[0].addCategory(Intent.CATEGORY_DEFAULT);
-		try {
-			filters[0].addDataType(MIME_TEXT_PLAIN);
-		} catch (MalformedMimeTypeException e) {
-			throw new RuntimeException("Check your mime type.");
-		}
-		 
-		adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
-	}
+        Charset utfEncoding = encodeInUtf8 ? Charset.forName("UTF-8") : Charset.forName("UTF-16");
+        byte[] textBytes = text.getBytes(utfEncoding);
 
-	public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-		adapter.disableForegroundDispatch(activity);
-	}
+        int utfBit = encodeInUtf8 ? 0 : (1 << 7);
+        char status = (char) (utfBit + langBytes.length);
 
-	private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
-	 
-	    @Override
-	    protected String doInBackground(Tag... params) {
-	        Tag tag = params[0];
-	         
-	        Ndef ndef = Ndef.get(tag);
-	        if (ndef == null) {
-	            // NDEF is not supported by this Tag. 
-	            return null;
-	        }
-	 
-	        NdefMessage ndefMessage = ndef.getCachedNdefMessage();
-	 
-	        NdefRecord[] records = ndefMessage.getRecords();
-	        for (NdefRecord ndefRecord : records) {
-	            if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
-	                try {
-	                    return readText(ndefRecord);
-	                } catch (UnsupportedEncodingException e) {
-	                    Log.e(TAG, "Unsupported Encoding", e);
-	                }
-	            }
-	        }
-	 
-	        return null;
-	    }
-	     
-	    private String readText(NdefRecord record) throws UnsupportedEncodingException { 
-	        byte[] payload = record.getPayload();
-	 
-	        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-	        int languageCodeLength = payload[0] & 0063;
+        byte[] data = new byte[1 + langBytes.length + textBytes.length];
+        data[0] = (byte) status;
+        System.arraycopy(langBytes, 0, data, 1, langBytes.length);
+        System.arraycopy(textBytes, 0, data, 1 + langBytes.length, textBytes.length);
 
-	        return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-	    }
-	     
-	    @Override
-	    protected void onPostExecute(String result) {
-	        if (result != null) {
-	            Log.e(TAG, result);
-	        }
-	    }
-	}
+        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], data);
+    }
+
+	private void resolveIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage[] msgs;
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            } else {
+                // Unknown tag type
+                byte[] empty = new byte[0];
+                byte[] id = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+                Parcelable tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                String test = dumpTagData(tag);
+                byte[] payload = test.getBytes();
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, id, payload);
+                NdefMessage msg = new NdefMessage(new NdefRecord[] { record });
+                msgs = new NdefMessage[] { msg };
+            }
+            // Setup the views
+           // buildStringTags(msgs);
+        }
+    }
+
+    private String dumpTagData(Parcelable p) {
+        StringBuilder sb = new StringBuilder();
+        Tag tag = (Tag) p;
+        byte[] id = tag.getId();
+        sb.append("Tag ID (hex): ").append(getHex(id)).append("\n");
+
+        Toast.makeText(this, "The tag is" + getHex(id), Toast.LENGTH_LONG).show();
+
+        sb.append("Tag ID (dec): ").append(getDec(id)).append("\n");
+        sb.append("ID (reversed): ").append(getReversed(id)).append("\n");
+
+        String prefix = "android.nfc.tech.";
+        sb.append("Technologies: ");
+        for (String tech : tag.getTechList()) {
+            sb.append(tech.substring(prefix.length()));
+            sb.append(", ");
+        }
+        sb.delete(sb.length() - 2, sb.length());
+        for (String tech : tag.getTechList()) {
+            if (tech.equals(MifareClassic.class.getName())) {
+                sb.append('\n');
+                MifareClassic mifareTag = MifareClassic.get(tag);
+                String type = "Unknown";
+                switch (mifareTag.getType()) {
+                case MifareClassic.TYPE_CLASSIC:
+                    type = "Classic";
+                    break;
+                case MifareClassic.TYPE_PLUS:
+                    type = "Plus";
+                    break;
+                case MifareClassic.TYPE_PRO:
+                    type = "Pro";
+                    break;
+                }
+                sb.append("Mifare Classic type: ");
+                sb.append(type);
+                sb.append('\n');
+
+                sb.append("Mifare size: ");
+                sb.append(mifareTag.getSize() + " bytes");
+                sb.append('\n');
+
+                sb.append("Mifare sectors: ");
+                sb.append(mifareTag.getSectorCount());
+                sb.append('\n');
+
+                sb.append("Mifare blocks: ");
+                sb.append(mifareTag.getBlockCount());
+            }
+
+            if (tech.equals(MifareUltralight.class.getName())) {
+                sb.append('\n');
+                MifareUltralight mifareUlTag = MifareUltralight.get(tag);
+                String type = "Unknown";
+                switch (mifareUlTag.getType()) {
+                case MifareUltralight.TYPE_ULTRALIGHT:
+                    type = "Ultralight";
+                    break;
+                case MifareUltralight.TYPE_ULTRALIGHT_C:
+                    type = "Ultralight C";
+                    break;
+                }
+                sb.append("Mifare Ultralight type: ");
+                sb.append(type);
+            }
+        }
+
+        return sb.toString();
+    }
+
+
+    private String getHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = bytes.length - 1; i >= 0; --i) {
+            int b = bytes[i] & 0xff;
+            if (b < 0x10)
+                sb.append('0');
+            sb.append(Integer.toHexString(b));
+            if (i > 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+
+    private long getDec(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = 0; i < bytes.length; ++i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
+    }
+
+    private long getReversed(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = bytes.length - 1; i >= 0; --i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
+    }
+
+    void buildStringTags(NdefMessage[] msgs) {
+        if (msgs == null || msgs.length == 0) {
+            return;
+        }
+        /*
+        // Parse the first message in the list
+        // Build views for all of the sub records
+        List<ParsedNdefRecord> records = NdefMessageParser.parse(msgs[0]);
+        final int size = records.size();
+        for (int i = 0; i < size; i++) {
+            ParsedNdefRecord record = records.get(i);
+            content.addView(record.getView(this, inflater, content, i), 1 + i);
+            content.addView(inflater.inflate(R.layout.tag_divider, content, false), 2 + i);
+        }
+        */
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        setIntent(intent);
+        resolveIntent(intent);
+    }
 }
